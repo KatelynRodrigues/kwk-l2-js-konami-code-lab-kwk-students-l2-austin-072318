49e887d124b02571e49bb60767bab01ab083de84
Katelyn Rodrigues <katelyn.rodrigues1@gmail.com>
2018-07-27 19:13:34 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -10,10 +10,14 @@ const codes = [
   "b",
   "a"
 ];
+// Keys for A, B, and C keys.
+const alphabet = ['a', 'b', 'c'];
+ 
+// Keep track of index outside of the event handler.
+let index = 0;
  
 // This is the function that would be invoked by the event listener.
 function onKeyDownHandler(e) {
-  let index = 0
   const key = e.key;
  
   if (key === codes[index]) {
