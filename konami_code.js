b5ea0e5e31ea0494206a327a9d1d13bccb8ca6da
Katelyn Rodrigues <katelyn.rodrigues1@gmail.com>
2018-07-27 19:13:13 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -10,14 +10,10 @@ const codes = [
   "b",
   "a"
 ];
-// Keys for A, B, and C keys.
-const alphabet = ['a', 'b', 'c'];
- 
-// Keep track of index outside of the event handler.
-let index = 0;
  
 // This is the function that would be invoked by the event listener.
 function onKeyDownHandler(e) {
+  let index = 0
   const key = e.key;
  
   if (key === codes[index]) {
@@ -33,44 +29,10 @@ function onKeyDownHandler(e) {
   }
 }
 
-
-
-
 const input = document.querySelector('body')
 
 input.addEventListener('keydown', function(e) {
   console.log(e.key)
   
   onKeyDownHandler(e)
-  
-  
-  
-  // if (e.key === "g") {
-  //   return e.preventDefault()
-  // }
 })
-
-// function init() {
-//   var konami = document.querySelector("body")
-//   konami.addEventListener("keydown",function())
-  
-  
-  
-  // let i = 0
-  // document.querySelector("body").addEventListener("keydown", function onKeyDownHandler(e){
-  //   const key = e.key
-  //   for (key in codes){
-  //     if (key===codes[i]){
-  //       i++
-  //       if (index === codes.length){
-  //         alert("Hurray!")
-  //         i = 0
-  //       }
-  //     } 
-      
-  //     else {
-  //       i = 0
-  //     }
-  //   }
-  // })
-//    }
