d7bfc2f5d410cfb0e1866e2cb2fa2ffd862fd16e
Katelyn Rodrigues <katelyn.rodrigues1@gmail.com>
2018-07-27 18:54:51 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -14,9 +14,21 @@ const codes = [
 function init() {
   let i = 0
   const konami = document.querySelector("body")
-  konami.addEventListener("keydown", onKeyDownHandler()){
-    
+  konami.addEventListener("keydown", function onKeyDownHandler(e){
+    const key = e.key
+    for (key in codes){
+      if (key===codes[i]){
+        i++
+        if (index === codes.length){
+          alert("Hurray!")
+          i = 0
+        }
+      } 
       
+      else {
+        alert("Please try again!")
+        i = 0
+      }
     }
   })
-}
+    }
